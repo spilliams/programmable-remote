a9087201c9c515787d8ace589872c12c2c0781c8
Spencer Williams <s@spencerenglish.com>
2024-05-04 19:39:34 -0700

    two leds

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import board
 import digitalio
 import time
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+red = digitalio.DigitalInOut(board.GP14)
+green = digitalio.DigitalInOut(board.GP15)
+red.direction = digitalio.Direction.OUTPUT
+green.direction = digitalio.Direction.OUTPUT
 
 while True:
-    led.value = True
+    red.value = False
+    green.value = True
     time.sleep(0.5)
-    led.value = False
+    red.value = True
+    green.value = False
     time.sleep(0.5)
