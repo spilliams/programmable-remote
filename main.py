7fd3b2fd5c520509acc30705536eb7414f8c1d39
Spencer Williams <s@spencerenglish.com>
2024-05-04 20:55:15 -0700

    read the encoder too

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 """Hello, World"""
 import board
 import digitalio
-# import rotaryio
+import rotaryio
 import time
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+ledBoard = digitalio.DigitalInOut(board.LED)
+ledRed = digitalio.DigitalInOut(board.GP12)
+ledGreen = digitalio.DigitalInOut(board.GP13)
+leds = [ledBoard, ledRed, ledGreen]
+for led in leds:
+    led.direction = digitalio.Direction.OUTPUT
 
-# rotary breakout COM is set to 3V3, so pull gpio down to read a button presses as "on"
+# rotary breakout COM is set to GND (so the encoder works), so pull gpio up to read a button presses as "on"
 
-# encA = digitalio.DigitalInOut(board.GP14)
-# encB = digitalio.DigitalInOut(board.GP15)
-# encoder = rotaryio.InrementalEncoder(encA, encB)
-# last_position = None
+encoder = rotaryio.IncrementalEncoder(board.GP14, board.GP15)
+last_position = 0
 
 buttonCenter = digitalio.DigitalInOut(board.GP20) # "SW1" on rotary breakout
 buttonDown = digitalio.DigitalInOut(board.GP19) # "SW2"
@@ -21,9 +23,20 @@ buttonUp = digitalio.DigitalInOut(board.GP17) # "SW4"
 buttonLeft = digitalio.DigitalInOut(board.GP16) # "SW5"
 buttons = [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]
 for button in buttons:
-    button.switch_to_input(pull=digitalio.Pull.DOWN)
+    button.switch_to_input(pull=digitalio.Pull.UP)
 
 
 while True:
-    led.value = buttonCenter.value or buttonUp.value or buttonDown.value or buttonLeft.value or buttonRight.value
-    time.sleep(0.1)
+    ledBoard.value = not (buttonCenter.value and buttonDown.value and buttonRight.value and buttonUp.value and buttonLeft.value)
+    
+    position = encoder.position
+    if last_position is None or position != last_position:
+        print("Position: {}".format(position))
+        ledGreen.value = position > last_position
+        ledRed.value = position < last_position
+        last_position = position
+    else:
+        ledGreen.value = False
+        ledRed.value = False
+    
+    time.sleep(0.01)
