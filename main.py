9505702d6c9cf92fc6b2bb6e25e9390be4494d7d
Spencer Williams <s@spencerenglish.com>
2024-05-04 20:24:44 -0700

    read some buttons

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 """Hello, World"""
 import board
 import digitalio
+# import rotaryio
 import time
 
-red = digitalio.DigitalInOut(board.GP14)
-green = digitalio.DigitalInOut(board.GP15)
-red.direction = digitalio.Direction.OUTPUT
-green.direction = digitalio.Direction.OUTPUT
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+# rotary breakout COM is set to 3V3, so pull gpio down to read a button presses as "on"
+
+# encA = digitalio.DigitalInOut(board.GP14)
+# encB = digitalio.DigitalInOut(board.GP15)
+# encoder = rotaryio.InrementalEncoder(encA, encB)
+# last_position = None
+
+buttonCenter = digitalio.DigitalInOut(board.GP20) # "SW1" on rotary breakout
+buttonDown = digitalio.DigitalInOut(board.GP19) # "SW2"
+buttonRight = digitalio.DigitalInOut(board.GP18) # "SW3"
+buttonUp = digitalio.DigitalInOut(board.GP17) # "SW4"
+buttonLeft = digitalio.DigitalInOut(board.GP16) # "SW5"
+buttons = [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]
+for button in buttons:
+    button.switch_to_input(pull=digitalio.Pull.DOWN)
+
 
 while True:
-    red.value = False
-    green.value = True
-    time.sleep(0.5)
-    red.value = True
-    green.value = False
-    time.sleep(0.5)
+    led.value = buttonCenter.value or buttonUp.value or buttonDown.value or buttonLeft.value or buttonRight.value
+    time.sleep(0.1)
