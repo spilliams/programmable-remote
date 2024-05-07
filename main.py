f725d6da43425178bf4e544b072d1d673370eab2
Spencer Williams <s@spencerenglish.com>
2024-05-06 17:30:34 -0700

    new breadboard, new wiring

--- main.py
+++ main.py
@@ -5,24 +5,22 @@ import rotaryio
 import time
 
 ledBoard = digitalio.DigitalInOut(board.LED)
-ledRed = digitalio.DigitalInOut(board.GP12)
-ledGreen = digitalio.DigitalInOut(board.GP13)
-leds = [ledBoard, ledRed, ledGreen]
-for led in leds:
+ledRed = digitalio.DigitalInOut(board.GP6)
+ledGreen = digitalio.DigitalInOut(board.GP5)
+for led in [ledBoard, ledRed, ledGreen]:
     led.direction = digitalio.Direction.OUTPUT
 
 # rotary breakout COM is set to GND (so the encoder works), so pull gpio up to read a button presses as "on"
 
-encoder = rotaryio.IncrementalEncoder(board.GP19, board.GP18)
+encoder = rotaryio.IncrementalEncoder(board.GP13, board.GP12)
 last_position = 0
 
-buttonCenter = digitalio.DigitalInOut(board.GP17) # "SW1" on rotary breakout
-buttonDown = digitalio.DigitalInOut(board.GP16) # "SW2"
-buttonRight = digitalio.DigitalInOut(board.GP27) # "SW3"
-buttonUp = digitalio.DigitalInOut(board.GP26) # "SW4"
-buttonLeft = digitalio.DigitalInOut(board.GP22) # "SW5"
-buttons = [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]
-for button in buttons:
+buttonCenter = digitalio.DigitalInOut(board.GP11) # "SW1" on rotary breakout
+buttonDown = digitalio.DigitalInOut(board.GP10) # "SW2"
+buttonRight = digitalio.DigitalInOut(board.GP9) # "SW3"
+buttonUp = digitalio.DigitalInOut(board.GP15) # "SW4"
+buttonLeft = digitalio.DigitalInOut(board.GP14) # "SW5"
+for button in [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]:
     button.switch_to_input(pull=digitalio.Pull.UP)
 
 
