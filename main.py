2f4fbe5deeccd5eb8c2be93e37a06659fafe5cfc
Spencer Williams <s@spencerenglish.com>
2024-05-24 14:39:09 -0700

    no more epd

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import digitalio
 import rotaryio
 import time
 
-import adafruit_ssd1681
-import displayio
-import busio
-from fourwire import FourWire
-
 ledBoard = digitalio.DigitalInOut(board.LED)
 ledRed = digitalio.DigitalInOut(board.GP17)
 ledGreen = digitalio.DigitalInOut(board.GP16)
@@ -27,36 +22,6 @@ buttonUp = digitalio.DigitalInOut(board.GP20) # "SW4"
 buttonLeft = digitalio.DigitalInOut(board.GP19) # "SW5"
 for button in [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]:
     button.switch_to_input(pull=digitalio.Pull.UP)
-
-# e-paper display pins
-# epd_sck = digitalio.DigitalInOut(board.GP14)
-# epd_miso = digitalio.DigitalInOut(board.GP12)
-# epd_mosi = digitalio.DigitalInOut(board.GP15)
-epd_ecs = digitalio.DigitalInOut(board.GP13)
-epd_dc = digitalio.DigitalInOut(board.GP7)
-# epd_srcs = digitalio.DigitalInOut(board.GP6)
-# epd_sdcs = digitalio.DigitalInOut(board.GP5)
-epd_reset = digitalio.DigitalInOut(board.GP4)
-epd_busy = digitalio.DigitalInOut(board.GP3)
-# epd_enable = digitalio.DigitalInOut(board.GP2)
-
-displayio.release_displays()
-spi = busio.SPI(clock=board.GP14, MOSI=board.GP15, MISO=board.GP12) # TypeError: command must be of type Pin, not DigitalInOut
-display_bus = FourWire(
-    spi, command=epd_dc, chip_select=epd_ecs, reset=epd_reset, baudrate=1000000
-)
-time.sleep(1)
-
-display = adafruit_ssd1681.SSD1681(
-    display_bus,
-    width=200,
-    height=200,
-    busy_pin=epd_busy,
-    highlight_color=0xFF0000,
-    rotation=180,
-)
-
-g = displayio.Group()
 
 while True:
     ledBoard.value = not (buttonCenter.value and buttonDown.value and buttonRight.value and buttonUp.value and buttonLeft.value)
