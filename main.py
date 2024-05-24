65faf52d0043b4bc4e1617bd2aaadcba897ffa7b
Spencer Williams <s@spencerenglish.com>
2024-05-24 14:57:26 -0700

    wire up the Sharp

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import digitalio
 import rotaryio
 import time
 
+import busio
+import displayio
+import framebufferio
+import sharpdisplay
+
 ledBoard = digitalio.DigitalInOut(board.LED)
 ledRed = digitalio.DigitalInOut(board.GP17)
 ledGreen = digitalio.DigitalInOut(board.GP16)
@@ -22,6 +27,11 @@ buttonUp = digitalio.DigitalInOut(board.GP20) # "SW4"
 buttonLeft = digitalio.DigitalInOut(board.GP19) # "SW5"
 for button in [buttonCenter, buttonDown, buttonRight, buttonUp, buttonLeft]:
     button.switch_to_input(pull=digitalio.Pull.UP)
+
+displayio.release_displays()
+spi = busio.SPI(clock=board.GP14, MOSI=board.GP15, MISO=board.GP12) # TypeError: command must be of type Pin, not DigitalInOut
+# For the 144x168 display (can be operated at up to 8MHz)
+framebuffer = sharpdisplay.SharpMemoryFramebuffer(spi, board.GP7, width=144, height=168, baudrate=8000000)
 
 while True:
     ledBoard.value = not (buttonCenter.value and buttonDown.value and buttonRight.value and buttonUp.value and buttonLeft.value)
